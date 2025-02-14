f7b041bb897bdb8dc1eae5490b509624c73ce4a2
Denismaka <makadenis370@gmail.com>
2025-02-14 21:34:41 +0100

    Mise en place du footer

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Pages</a>
+                            <a class="nav-link text-white" href="#">Projets</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white" href="#">Contact</a>
@@ -312,6 +312,77 @@
             </div>
         </div>
     </section>
+
+    <!-- ################  -->
+    <!-- Footer  -->
+    <!-- ################  -->
+    <footer class="text-white" style="background-color: #000;">
+        <div class="container">
+            <footer class="py-5" style="margin-top: 10rem;">
+                <div class="row">
+                    <div class="col-6 col-md-3 mb-3">
+                        <h5>Liens</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Home</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">About</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Services</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Projets</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Services</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Conception et planification</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Construction et réalisation</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Gestion de chantier</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Services aux clients</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Expertise et innovation</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-5 offset-md-1 mb-3">
+                        <form>
+                            <h5>Abonnez-vous à notre newsletter.</h5>
+                            <p>Résumé mensuel de ce qui est nouveau et excitant chez nous.</p>
+                            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                                <label for="newsletter1" class="visually-hidden">Email address</label>
+                                <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                                <button class="btn" type="button"><a href="#" class="text-decoration-none text-white">Subscribe</a></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                    <p>&copy; 2025 Design by <a href="#" class="text-decoration-none text-white">Denismaka</a>, Tout droit réservé</p>
+                    <ul class="list-unstyled d-flex">
+                        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                                    <use xlink:href="#twitter" />
+                                </svg></a></li>
+                        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                                    <use xlink:href="#instagram" />
+                                </svg></a></li>
+                        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                                    <use xlink:href="#facebook" />
+                                </svg></a></li>
+                    </ul>
+                </div>
+            </footer>
+
+            <!-- Bouton de redirection vers le haut -->
+            <div onclick="window.scrollTo({top: 0, behavior: 'smooth'});" style="height: 50px; width: 50px; background-color: #ff8400; border-radius: 50%; display: flex; justify-content: center; align-items: center; position: fixed; right: 20px; bottom: 20px; cursor: pointer;">
+                <img src="/assets/images/arrow-up-solid-svgrepo-com.svg" alt="Retour en haut" class="icone" style="width: 30px; height: 30px;">
+            </div>
+
+            <style>
+                .icone {
+                    filter: brightness(0) invert(1);
+                }
+            </style>
+
+    </footer>
 
 
 
