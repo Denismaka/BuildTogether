0d1c9b0a44003816c6eb85b361d36ad4923ff533
Denismaka <makadenis370@gmail.com>
2025-02-14 14:12:01 +0100

    correction de structure du Header et de la section Home

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/fonts/css/all.min.css">
     <link rel="stylesheet" href="/assets/Js/app.js">
     <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="website icon" href="/assets/images/website.png" />
+    <link rel="website icon" href="/assets/images/Patmos_Logo_2544cfe63a.webp" />
     <link rel="stylesheet" href="/assets/swiper/package/swiper-bundle.css">
+    <link rel="stylesheet" href="/assets/scrollreveal/test">
     <title>BuildTogether</title>
 </head>
 
@@ -18,45 +19,43 @@
     <!-- ############## -->
     <!-- NavBar -->
     <!-- ############## -->
-    <nav class="navbar navbar-expand-lg " style="z-index: 1000;">
-        <div class="container-fluid">
-            <a class="navbar-brand mx-5" href="#">Build<span>together</span></a>
+    <nav class="navbar navbar-expand-lg" style="z-index: 1000;">
+        <div class="container">
+            <a class="navbar-brand mx-5 text-white text-uppercase fw-bold" href="#">Build <span style="color: #ff8400;">together</span></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <div class="mx-auto"> <!-- Conteneur pour centrer -->
-                    <ul class="navbar-nav">
+                <div class="mx-auto">
+                    <ul class="navbar-nav fw-bold">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
+                            <a class="nav-link text-white" href="#">About</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Services
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item " style="color: #000;" href="#">Action</a></li>
+                                <li><a class="dropdown-item " style="color: #000;" href="#">Another action</a></li>
+                                <li><a class="dropdown-item " style="color: #000;" href="#">Something else here</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Pages</a>
+                            <a class="nav-link text-white" href="#">Pages</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
+                            <a class="nav-link text-white" href="#">Contact</a>
                         </li>
                     </ul>
                 </div>
-                <div class="mx-5">
+                <div class="button mx-5">
                     <form class="d-flex" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">
-                            <a href="#" class="text-decoration-none">Search</a>
-                        </button>
+                        <a href="#" class="btn text-decoration-none text-white" style="background-color: #ff8400;">Search</a>
                     </form>
                 </div>
             </div>
@@ -70,7 +69,7 @@
         <div id="carouselExample" class="carousel slide">
             <div class="carousel-inner">
                 <div class="carousel-item active" style="height: 92vh;">
-                    <img src="/assets/images/banner.jpg" class="d-block w-100" alt="..." style="filter: brightness(90%);">
+                    <img src="/assets/images/banner.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%);">
                     <div class="carousel-caption d-none d-md-block" style=" top: 14rem; left: 0; transform: translate(0, 0);">
                         <h1 class="text-capitalize">votre désir, notre défi</h1>
                         <h2>Parce que nous savons à quel point c'est important pour vous.</h2>
@@ -105,8 +104,10 @@
 
 
 
+
+
     <script src="/assets/swiper/package/swiper-bundle.min.js"></script>
-    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="/assets/Js/script.js"></script>
     <!-- <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script> -->
     <script src="/assets/Js/app.js"></script>
