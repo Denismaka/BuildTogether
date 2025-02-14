2aa3faa3d5152194ed4226ea526a06510013fb47
Denismaka <makadenis370@gmail.com>
2025-02-14 12:29:53 +0100

    Debut du projet

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/fonts/css/all.min.css">
+    <link rel="stylesheet" href="/assets/Js/app.js">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="website icon" href="/assets/images/website.png" />
+    <link rel="stylesheet" href="/assets/swiper/package/swiper-bundle.css">
+    <title>BuildTogether</title>
+</head>
+
+<body>
+    <!-- ############## -->
+    <!-- NavBar -->
+    <!-- ############## -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+    <script src="/assets/swiper/package/swiper-bundle.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/Js/script.js"></script>
+    <!-- <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script> -->
+    <script src="/assets/Js/app.js"></script>
+</body>
+
+</html>
