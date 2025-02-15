23afed9c56a427de479371dd17cb6f6c3cba2464
Denismaka <makadenis370@gmail.com>
2025-02-15 13:44:49 +0100

    correction de l'emplacement du script de scrollreveal

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/font/css/all.min.css">
-    <link rel="stylesheet" href="/assets/Js/app.js">
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="website icon" href="/assets/images/Patmos_Logo_2544cfe63a.webp" />
     <link rel="stylesheet" href="/assets/swiper/package/swiper-bundle.css">
-    <link rel="stylesheet" href="/assets/scrollreveal/test">
     <title>BuildTogether</title>
 </head>
 
@@ -388,8 +386,7 @@
 
     <script src="/assets/swiper/package/swiper-bundle.min.js"></script>
     <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/Js/script.js"></script>
-    <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script>
+    <!-- <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script> -->
     <script src="/assets/Js/app.js"></script>
 </body>
 
